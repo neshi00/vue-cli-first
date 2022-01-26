8810a4c9a01e45a61ae23f6917719b1b757baeda
neshi00 <luca.ascione85@gmail.com>
2022-01-26 17:29:25 +0100

    title component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Title />
+  </div>
 </template>
 
 <script>
+import Title from "./components/Title.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Title,
+  },
 };
 </script>
 
---
+++ src/components/Title.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="titolo">La mia prima app con Vue CLI!</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.titolo {
+  font-size: 40px;
+  font-weight: bold;
+}
+</style>
